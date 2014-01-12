jstralko/PennaFlame-ios

commit 200dfca0bf603f75094c148b01a45b5df34c0f59

Reorder the Hardness Chart Keys.
This order is purely business logic, so I had
to hard code the order and pass it to the various
ViewControllers for display purposes.

PennaFlame/PFFullChartViewController.m
@@ -34,9 +34,14 @@
 }
 
 -(id) initWithDict:(NSDictionary *)chartDict withTile:(NSString *)title {
+    return [self initWithDict:chartDict withSortedKeys:[chartDict allKeys] withTile:title];
+}
+
+-(id) initWithDict:(NSDictionary *)chartDict withSortedKeys:(NSArray *)keys withTile:(NSString *)title {
     self = [super init];
     if (self) {
         chartDictionary = chartDict;
+        sortedKeys = keys;
         self.navigationItem.title = title;
     }
     
@@ -329,7 +334,6 @@
 }
 
 - (NSString *)generateHeaderChartFromDictionary {
-    NSArray *keys = [chartDictionary allKeys];
     NSMutableString *html = [NSMutableString stringWithFormat:@"<html><head>"
                              "</head>"
                              "<body style=\"bbackground-color: transparent;\">"
@@ -337,7 +341,7 @@
                              "<thead>"
                              "<tr bgcolor=\"lightgrey\" align=\"center\">"];
     
-    for (NSString *key in keys) {
+    for (NSString *key in sortedKeys) {
         [html appendFormat:@"<th bgcolor=\"#FF0000\"><span style=\"font-weight:bold;font-size:0.50em\">%@</span></th>", key];
     }
     [html appendFormat:@"</tr></thread>"];
@@ -348,37 +352,37 @@
 }
 
 -(NSString *) generateChartFromDictionary {
-    NSArray *keys = [chartDictionary allKeys];
     NSMutableString *html = [NSMutableString stringWithFormat:@"<html><head>"
                              "</head>"
                              "<body style=\"bbackground-color: transparent;\">"
                              "<table id=\"myTable\" width=\"90%%\" border=\"1\" align=\"center\" cellpadding=\"3\" cellspacing=\"0\" bordercolor=\"#CCCCC\">"
-                             "<thead id=\"myTableHeader\" style=\"height:1px\">"
+                             "<thead id=\"myTableHeader\">"
                              "<tr bgcolor=\"transparent\" align=\"center\">"];
     
-    for (NSString *key in keys) {
+    for (NSString *key in sortedKeys) {
         [html appendFormat:@"<th bgcolor=\"transparent\"><span style=\"opacity:0;font-weight:bold;font-size:0.50em\">%@</span></th>", key];
     }
     [html appendFormat:@"</tr></thread>"];
     [html appendFormat:@"<tbody>"];
-    NSUInteger total = [[chartDictionary objectForKey:[keys objectAtIndex:0]] count];
+    NSUInteger total = [[chartDictionary objectForKey:[sortedKeys objectAtIndex:0]] count];
     
     for (int i = 0 ; i < total; i++) {
         [html appendFormat:@"<tr bgcolor=\"white\">"];
-        for (NSString *key in keys) {
+        for (NSString *key in sortedKeys) {
             NSString *range = [[chartDictionary objectForKey:key] objectAtIndex:i];
             [html appendFormat:@"<td><div align=\"center\">%@</div></td>", range];
         }
         [html appendFormat:@"</tr>"];
     }
-    [html appendFormat:@"</tbody></table></body></html>"];
+    [html appendFormat:@"</tbody></table>"];
+    [html appendFormat:@"<span style=\"margin-left:5%%;margin-right:5%%;font-size:0.8em;font-style:italic;\">%@</span>", disclaimerString];
+    [html appendFormat:@"</body></html>"];
+    
     return html;
 }
 
 - (void) webViewDidFinishLoad:(UIWebView *)webView
-{
-    NSLog(@"webviewDidFinished");
-    
+{    
     //load the jquery framework
     NSString *jqueryCore = [NSString stringWithFormat:@"%@", [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"jquery-1.10.2.min.js"]];
     

PennaFlame/PFAppDelegate.h
@@ -18,6 +18,7 @@
 @property (strong, nonatomic) UIWindow *window;
 @property (readonly) NSMutableDictionary *hardnessCaseDepthChartDict;
 @property (readonly) NSMutableDictionary *hardnessChartDict;
+@property (readonly) NSArray *hardnessChartSortedKeys;
 
 @end
 

PennaFlame/PFAppDelegate.m
@@ -19,6 +19,7 @@
 
 @synthesize hardnessCaseDepthChartDict;
 @synthesize hardnessChartDict;
+@synthesize hardnessChartSortedKeys;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
@@ -32,6 +33,15 @@
     
     plistPath = [[NSBundle mainBundle] pathForResource:@"HardnessChartData" ofType:@"plist"];
     hardnessChartDict = [NSMutableDictionary dictionaryWithContentsOfFile:plistPath];
+    
+    NSMutableArray *sortedKeys = [NSMutableArray array];
+    [sortedKeys addObject:@"EQUO TIP Ld"];
+    [sortedKeys addObject:@"ROCKWELL C HRC"];
+    [sortedKeys addObject:@"BRINELL BHN"];
+    [sortedKeys addObject:@"SHORE C HSc"];
+    [sortedKeys addObject:@"SHORE C HFRSc"];
+    [sortedKeys addObject:@"VICKERS HV/DPH"];
+    hardnessChartSortedKeys = [[NSArray alloc] initWithArray:sortedKeys];
     
     //New UICollectionViewController - new and shiney!
     PFHomeViewController *home = [[PFHomeViewController alloc] init];

PennaFlame/PFChartViewController.h
@@ -26,9 +26,12 @@
     NSLayoutConstraint *logoImageViewTop;
     int tabBarHeight;
     NSMutableDictionary *chartDictionary;
+    NSArray *sortedKeys;
     NSLayoutConstraint *webViewHeightConstraint;
 }
 
 -(id)initWithDict:(NSMutableDictionary *)chartDict withTitle:(NSString *)title;
+
+-(id)initWithDict:(NSMutableDictionary *)chartDict sortedKeys:(NSArray *)keys withTitle:(NSString *)title;
 
 @end

PennaFlame/PFChartViewController.m
@@ -27,9 +27,14 @@
 }
 
 -(id)initWithDict:(NSMutableDictionary *)chartDict withTitle:(NSString *)title {
+    return [self initWithDict:chartDict sortedKeys:[chartDict allKeys] withTitle:title];
+}
+
+-(id)initWithDict:(NSMutableDictionary *)chartDict sortedKeys:(NSArray *)keys withTitle:(NSString *)title {
     self = [super init];
     if (self) {
         chartDictionary = chartDict;
+        sortedKeys = keys;
         self.navigationItem.title = title;
     }
     return self;
@@ -185,7 +190,8 @@
         }
         [rangePicker setHidden:![rangePicker isHidden]];
     } else if (sender == showFullChart) {
-        PFFullChartViewController *fcvc = [[PFFullChartViewController alloc ] initWithDict:chartDictionary];
+        PFFullChartViewController *fcvc = [[PFFullChartViewController alloc ] initWithDict:chartDictionary
+                                                                            withSortedKeys:sortedKeys withTile:self.navigationItem.title];
         [self.navigationController pushViewController:fcvc animated:YES];
         
     } else {
@@ -194,7 +200,6 @@
         
         NSInteger metalIndex = [topPicker selectedRowInComponent:0];
         NSString *metal = [[chartDictionary allKeys] objectAtIndex:metalIndex];
-        NSArray *metals = [chartDictionary allKeys];
         //NSString *range = [[hardnessChartDict objectForKey:metal] objectAtIndex:[rangePicker selectedRowInComponent:0]];
         
         /*
@@ -219,7 +224,7 @@
                                  "<tbody>"
                                  "<tr bgcolor=\"lightgrey\" align=\"center\">" ];
         
-        for (NSString *key in metals) {
+        for (NSString *key in sortedKeys) {
             if ([metal isEqualToString:key]) {
                 [html appendFormat:@"<td bgcolor=\"#FF0000\"><span style=\"font-weight:bold\">%@</span></td>", key];
                 
@@ -228,12 +233,10 @@
             }
         }
         [html appendFormat:@"</tr><tr bgcolor=\"white\">"];
-        for (NSString *key in metals) {
+        for (NSString *key in sortedKeys) {
             [html appendFormat:@"<td><div align=\"center\">%@</div></td>", [[chartDictionary objectForKey:key] objectAtIndex:[rangePicker selectedRowInComponent:0]]];
         }
         [html appendFormat:@"</tr></table></body></html>"];
-        
-        //NSLog(@"%@", html);
         
         [chartWebView loadHTMLString:html baseURL:nil];
         [chartWebView setHidden:NO];
@@ -741,12 +744,11 @@
 -(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row   forComponent:(NSInteger)component
 {
     if (pickerView == topPicker) {
-        NSArray *array = [chartDictionary allKeys];
-        return [array objectAtIndex:row];
+        return [sortedKeys objectAtIndex:row];
     }
     
     NSInteger index = [topPicker selectedRowInComponent:0];
-    NSString *key = [[chartDictionary allKeys] objectAtIndex:index];
+    NSString *key = [sortedKeys objectAtIndex:index];
     NSArray *array = [chartDictionary objectForKey:key];
     return [array objectAtIndex:row];
     

PennaFlame/PFFullChartViewController.h
@@ -16,6 +16,7 @@
     UIView *redBanner;
     UILabel *disclaimer;
     NSDictionary *chartDictionary;
+    NSArray *sortedKeys;
     NSInteger tabBarHeight;
     PFTabView *tabBar;
 }
@@ -23,5 +24,7 @@
 -(id) initWithDict:(NSDictionary *)chartDict;
 
 -(id) initWithDict:(NSDictionary *)chartDict withTile:(NSString *)title;
+
+-(id) initWithDict:(NSDictionary *)chartDict withSortedKeys:(NSArray *)keys withTile:(NSString *)title;
 
 @end

PennaFlame/PFHomeViewController.m
@@ -207,7 +207,9 @@
             break;
         case 3: {
             PFAppDelegate *appDelegate = (PFAppDelegate *)[UIApplication sharedApplication].delegate;
-            PFChartViewController *pfhcvc = [[PFChartViewController alloc]initWithDict:appDelegate.hardnessChartDict withTitle:HARDNESS_CHART_TITLE];
+            PFChartViewController *pfhcvc = [[PFChartViewController alloc]initWithDict:appDelegate.hardnessChartDict
+                                            sortedKeys:appDelegate.hardnessChartSortedKeys
+                                            withTitle:HARDNESS_CHART_TITLE];
             [self.navigationController pushViewController:pfhcvc animated:YES];
         }
             break;

commit f073ac66fc7fb6c229b65b5a984110a7ecc73886

Set the fontSize depending on the device.
Also this fixed a compiler error introduced
in commit: 200dfca.

PennaFlame/PFFullChartViewController.m
@@ -10,6 +10,8 @@
 #import "PFAppDelegate.h"
 #import "PFTabView.h"
 
+static NSString *disclaimerString = @"Note: This chart is a general guide. Hardness and case depth's may vary depending on the flame hardening technique used and actual chemistry of the material.";
+
 @interface PFFullChartViewController ()
 
 - (NSString *) generateChartFromDictionary;
@@ -68,6 +70,7 @@
     [chart loadHTMLString:html baseURL:nil];
     [chart setBackgroundColor:[UIColor clearColor]];
     [chart setOpaque:NO];
+    chart.scrollView.bounces = NO;
     chart.delegate = self;
     [self.view addSubview:chart];
     
@@ -79,7 +82,6 @@
     [header_internalChart setOpaque:NO];
     header_internalChart.scrollView.scrollEnabled = NO;
     header_internalChart.scrollView.bounces = NO;
-    //chart.delegate = self;
     [self.view addSubview:header_internalChart];
     
     disclaimer = [[UILabel alloc] initWithFrame:CGRectZero];
@@ -90,7 +92,7 @@
     disclaimer.adjustsFontSizeToFitWidth = YES;
     disclaimer.font = [UIFont italicSystemFontOfSize:12];
     disclaimer.minimumScaleFactor = 0.5f;
-    [disclaimer setText:@"Note: This chart is a general guide. Hardness and case depth's may vary depending on the flame hardening technique used and actual chemistry of the material."];
+    [disclaimer setText:disclaimerString];
     [self.view addSubview:disclaimer];
     
     if ([self.navigationItem.title isEqualToString:HARDNESS_CASE_DEPTH_TITLE]) {
@@ -215,7 +217,7 @@
                    toItem:redBanner
                    attribute:NSLayoutAttributeBottom
                    multiplier:1.0
-                   constant:15];
+                   constant:5];
     [self.view addConstraint:myConstraint];
     
     myConstraint =[NSLayoutConstraint
@@ -256,7 +258,7 @@
                    toItem:redBanner
                    attribute:NSLayoutAttributeBottom
                    multiplier:1.0
-                   constant:15];
+                   constant:10];
     [self.view addConstraint:myConstraint];
     
     myConstraint =[NSLayoutConstraint
@@ -287,7 +289,7 @@
                    toItem:chart
                    attribute:NSLayoutAttributeBottom
                    multiplier:1.0
-                   constant:5];
+                   constant:0];
     [self.view addConstraint:myConstraint];
     
     myConstraint =[NSLayoutConstraint
@@ -341,8 +343,16 @@
                              "<thead>"
                              "<tr bgcolor=\"lightgrey\" align=\"center\">"];
     
+    NSString *fontSize;
+    if ([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPad) {
+        //iPad
+        fontSize = @"1.0em";
+    } else {
+        fontSize = @"0.50em";
+    }
+    
     for (NSString *key in sortedKeys) {
-        [html appendFormat:@"<th bgcolor=\"#FF0000\"><span style=\"font-weight:bold;font-size:0.50em\">%@</span></th>", key];
+        [html appendFormat:@"<th bgcolor=\"#FF0000\"><span style=\"font-weight:bold;font-size:%@\">%@</span></th>", fontSize, key];
     }
     [html appendFormat:@"</tr></thread>"];
     [html appendFormat:@"<tbody>"];